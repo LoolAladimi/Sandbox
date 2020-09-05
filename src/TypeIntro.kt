5ed4c511201126b0b3b80b2efd08eda6da8ca350
lool-aladimi <loolaladimi@gmail.com>
2020-09-06 00:23:46 +0300

    Initial commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,23 @@
+class TypeIntro {
+    fun main(){
+        var experiencePoints:Int = 10;
+        println(experiencePoints);
+
+        val playerName = "Estragon";
+        println(playerName);
+
+        var magic = playerName.reversed()
+        println(magic)
+
+        var hasSteed = true
+        if(hasSteed == true){
+            println("They  have dragon")
+
+        }else{
+            println("They dont have dragon")
+        }
+
+
+
+    }
+}
